572f4db8a309159dcc0acd62cda4ce9798ed6eb5
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-08-29 17:14:21 -0500

    navbar and container are showing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import NavBar from "./components/layout/NavBar";
+import Dashboard from "./components/layout/Dashboard";
 
 class App extends Component {
     render() {
-        return <div className="App">
-            <h1>Test</h1>
-        </div>
+        return (
+            <div className="App">
+                <NavBar/>
+                <div className="container"><Dashboard/></div>
+            </div>
+        );
     }
 }
 
