30ba57b675237c688b600598b410d4e729fec9ca
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-08-30 10:48:44 -0500

    cards are clickable and will forward to 'more info'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, {Component} from 'react';
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css'
 import NavBar from "./components/layout/NavBar";
 import Dashboard from "./components/layout/Dashboard";
+import Pokemon from "./components/pokemon/Pokemon";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <NavBar/>
-                <div className="container"><Dashboard/></div>
-            </div>
+            <Router>
+                <div className="App">
+                    <NavBar />
+                    <div className="container">
+                        <Switch>
+                            <Route exact path="/" component={Dashboard} />
+                            <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+                        </Switch>
+                    </div>
+                </div>
+            </Router>
         );
     }
 }
