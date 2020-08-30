3d723059aa76e319945f99550b63f6a79e46d0f1
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-08-30 10:49:23 -0500

    styling changes

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react';
+import {Link} from "react-router-dom";
 import styled from "styled-components";
 
 const Sprite = styled.img`
 width: 5em;
 height: 5em;
-// display: none;
+`;
+
+const Card = styled.div`
+&:hover{
+    transform: scale(1.05);
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+}
+`;
+
+const StyledLink = styled(Link)`
+text-decoration: none;
+color: black;
+&:focus,
+&:hover,
+&:visited,
+&:link,
+&:active{
+text-decoration: none;
+}
 `;
 
 class PokemonCard extends Component {
@@ -30,12 +50,15 @@ class PokemonCard extends Component {
 
         return (
             <div className='col-md-3 col-sm-6 mb-5'>
-                <div className='card text-capitalize'>
-                    <div className='card-header'><h5>#{this.state.pokemonIndex.toString().padStart(3, '0')}</h5></div>
-                    <Sprite className='card-img-top rounded mx-auto mt-2' src={this.state.imageUrl}>
-                    </Sprite>
-                    <div className='card-body mx-auto'><h6 className='card-title'>{this.state.name}</h6></div>
-                </div>
+                <StyledLink to={`pokemon/${this.state.pokemonIndex}`}>
+                    <Card className='card text-capitalize'>
+                        <div className='card-header'><h5>#{this.state.pokemonIndex.toString().padStart(3, '0')}</h5>
+                        </div>
+                        <Sprite className='card-img-top rounded mx-auto mt-2' src={this.state.imageUrl}>
+                        </Sprite>
+                        <div className='card-body mx-auto'><h5 className='card-title'>{this.state.name}</h5></div>
+                    </Card>
+                </StyledLink>
             </div>
         );
     }
