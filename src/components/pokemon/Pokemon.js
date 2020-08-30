fb604e818294964b07d058e4e52cbd814656e06f
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-08-30 10:49:53 -0500

    working on fetching individual data

---
+++ src/components/pokemon/Pokemon.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class Pokemon extends Component {
+    state = {
+        name: '',
+        imageUrl: '',
+        pokemonIndex: '',
+        types: [],
+        description: '',
+        stats: {
+            hp: '',
+            attack: '',
+            defense: '',
+            speed: '',
+            specialAttack: '',
+            specialDefense: ''
+        },
+        height: '',
+        weight: '',
+        eggGroup: '',
+        abilities: '',
+        genderRatioMale: '',
+        genderRatioFemale: '',
+        evs: '',
+        hatchSteps: ''
+    };
+
+    async componentDidMount() {
+        const {pokemonIndex} = this.props.match.params;
+
+        const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`;
+        const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}/`;
+
+        const pokemonRes = await axios.get(pokemonUrl);
+
+        const name = pokemonRes.data.name;
+        const imageUrl = pokemonRes.data.sprites.front_default;
+
+        let {hp, attack, defense, speed, specialAttack, specialDefense} = '';
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.name}</h1>
+            </div>
+        );
+    }
+}
+
+export default Pokemon;
